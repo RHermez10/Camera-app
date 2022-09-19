efd86a0f5bb5170f8fb0e2bb0ee857dc503b0215
Rhermez10 <ramta97@hotmail.com>
2022-09-19 17:04:05 +0200

    camera-test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Camera from './Components/Camera';
 
 function App() {
   return (
     <div className="App">
-
+<Camera/>
     </div>
   );
 }
